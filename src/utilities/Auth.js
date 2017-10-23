87ad23915f8cb04743307e02f033ed6cb6de9550
Steffen Thomsen <acbolic@gmail.com>
2017-10-23 22:08:49 +0200

    Clean repo

--- src/utilities/Auth.js
+++
@@ -1,9 +0,0 @@
-export default {
-  auth: {
-    header: () => {
-      // return authorization header with jwt token
-      const token = localStorage.getItem("gozoneJWT");
-      return token ? { Authorization: `Bearer ${token}` } : {};
-    }
-  }
-};
